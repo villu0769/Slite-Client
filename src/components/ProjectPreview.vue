08ad97b7e1e9cbd81777a47db017f122d5b89262
villu0769 <119662188+villu0769@users.noreply.github.com>
2026-01-26 12:56:35 +0200

    improved project preview

--- src/components/ProjectPreview.vue
+++ src/components/ProjectPreview.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="project-box" @click="handleOnClick(projectData._id)">
-    <p>{{ projectData.name }}</p>
-  </div>
-</template>
+  <Sidebar @start-drag="onStartDragFromMenu" />
 
+ <div id="header">
+    <div class="header-info">
+      <div class="header-preview">
+        <img :src="projectImage || 'https://via.placeholder.com/150'" alt="Project Preview" />
+      </div>
+      
+      <span class="header-title">{{ name || 'Untitled Project' }}</span>
+    </div>
+
+    <div class="header-actions">
+      <button class="action-btn delete-btn" @click="deleteProject" title="Delete Project">
+        <svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <polyline points="3 6 5 6 21 6"></polyline>
+          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+        </svg>
+      </button>
+    </div>
+  </div>
+
+  <EditProject v-if="projectData" :projectData="projectData" ref="editProjectRef" />
+</template>
 <script setup>
 import { useRouter } from 'vue-router'
 
